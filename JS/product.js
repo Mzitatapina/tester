5168b6ace8b2dfb389c0e79063d10577aadb8389
Mzitatapina <pikrialomjaria@gmail.com>
2024-08-31 20:54:36 +0400

    Update product.js

--- JS/product.js
+++ JS/product.js
@@ -10,14 +10,12 @@ async function openModal(modalId) {
     modalContent.style.transform = "translateY(-20px)"; // Slightly move up
 
     setTimeout(function() {
-        modalContent.style.opacity = "0.7"; // Fade in the content
+        modalContent.style.opacity = "0.3"; // Fade in the content
+        modalContent.style.opacity = "0.5"
+        modalContent.style.opacity = "1"
         modalContent.style.transform = "translateY(0)"; // Move to the normal position
     }, 300); // Delay in milliseconds (500ms = 0.5s)
 
-    setTimeout(function() {
-        modalContent.style.opacity = "1"; // Fade in the content
-        modalContent.style.transform = "translateY(0)"; // Move to the normal position
-    }, 300); // Delay in milliseconds (500ms = 0.5s)
 }
 
 // Function to close the modal
